6b4df90011cc069610f3f9a4ad1ed313fcc63674
Bradly Horn <wolfnchaos@gmail.com>
2020-08-10 21:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function doToElementsInArray() {
-
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
 }
 
 
-function changeCompletely() {
-
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
+ 
+var animals = ["dog", "cat", "squirrel"];
+ 
+doToElementsInArray(animals, changeCompletely);
